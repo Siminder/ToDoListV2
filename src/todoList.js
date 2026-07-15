6b307b7fcc1af8df21ce57306912fd80aa666005
Siminder Bansal <w0654525@myscc.ca>
2026-07-15 18:49:47 -0400

    add base structrue and webpack added styles to layout the structor, added html structor, added base js to start working on fucntion

--- src/todoList.js
+++ src/todoList.js
@@ -1,4 +1,19 @@
-import "./styles.css";
 
 
+//takes input from input bar and makes it into a li element in the selected element
+
+const input = document.querySelector("#todoSearch")
+let submit = document.querySelector("#submit");
+let todoList = document.querySelector("#todoItems")
+
+
+
+submit.addEventListener("click", function (e) {
+     if(input){
+       const newli = document.createElement("li");
+       newli.textContent = input.value;
+       todoList.appendChild(newli)
+
+    }
+})
 
